90b557d52a5b5c15dc1f78c4959efe6b239c9052
DE\MIR6SI <fixed-term.AhmadHassan.Mirza@de.bosch.com>
2020-06-09 18:54:16 +0200

    word count function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 /* copy input to output; 1st version */
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
 
 int fahrToCelsius(){
     int f,c;
@@ -28,14 +30,34 @@ int readChar(){
 
 int power(int ,int);
 
+int countWords()
+{
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == '\n')
+            break;
+        if (c == ' ' || c == '\n' || c == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+}
+
 main()
 {
     fahrToCelsius();
     //readChar();
     int base=2;
     int n=2;
-    int a= power(2,2)
+    int a= power(2,2);
     printf("%d raised to the power %d = %d\n",base,n, a);
+    countWords();
 }
 
 int power(int base, int n)
